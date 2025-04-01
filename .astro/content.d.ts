b844bba17a8db8d07c96b9dd60439cf5250b8c66
Jmoreno003 <joseemorenoo003@gmail.com>
2025-04-01 02:48:45 +0200

    reload en los botones

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -157,6 +157,14 @@ declare module 'astro:content' {
   rendered?: RenderedContent;
   filePath?: string;
 }>;
+"equipos": Record<string, {
+  id: string;
+  body?: string;
+  collection: "equipos";
+  data: any;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
 "pilotos": Record<string, {
   id: string;
   render(): Render[".md"];
